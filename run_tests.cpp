d821bc3736766e269d0c11ea24f44d0bd962c4c4
Steven Swanson <swanson@cs.ucsd.edu>
2020-02-18 19:43:18 +0000

    Actually enable the tests for the functions we are working on

--- run_tests.cpp
+++ run_tests.cpp
@@ -15,99 +15,49 @@ namespace Tests {
 	};
 
 	TEST_F(OptimizationTests, level_0_fc) {
-		fc_test_activate<opt_fc_layer_t>(1,1,1,1,1,1);
-		//fc_test_calc_grads<opt_fc_layer_t>(1,1,1,1,1,1);
+		fc_test_activate<opt_fc_layer_t>   (1,1,1,1,1,1);
+		fc_test_calc_grads<opt_fc_layer_t> (1,1,1,1,1,1);
 		fc_test_fix_weights<opt_fc_layer_t>(1,1,1,1,1,1);
-		fc_test<opt_fc_layer_t>(1,1,1,1,1,1);
+		fc_test<opt_fc_layer_t>            (1,1,1,1,1,1);
 	}	
 			  
 	TEST_F(OptimizationTests, level_1_fc) {
-		fc_test_activate<opt_fc_layer_t>(4,  4,  4,  4, 4, 1);
-		fc_test_activate<opt_fc_layer_t>(4,  4,  2,  2, 8, 1);
-		fc_test_activate<opt_fc_layer_t>(8,  8,  2,  2, 16,1);
-		fc_test_activate<opt_fc_layer_t>(32, 32, 8,  8, 4, 1);
-		fc_test_activate<opt_fc_layer_t>(64, 64, 16, 8, 8, 1);
-
-#if (0)
-		fc_test_calc_grads<opt_fc_layer_t>(4,  4,  4,  4, 4, 1);
-		fc_test_calc_grads<opt_fc_layer_t>(4,  4,  2,  2, 8, 1);
-		fc_test_calc_grads<opt_fc_layer_t>(8,  8,  2,  2, 16,1);
-		fc_test_calc_grads<opt_fc_layer_t>(32, 32, 8,  8, 4, 1);
-		fc_test_calc_grads<opt_fc_layer_t>(64, 64, 16, 8, 8, 1);
-
-		fc_test_fix_weights<opt_fc_layer_t>(4,  4,  4,  4, 4, 1);
-		fc_test_fix_weights<opt_fc_layer_t>(4,  4,  2,  2, 8, 1);
-		fc_test_fix_weights<opt_fc_layer_t>(8,  8,  2,  2, 16,1);
-		fc_test_fix_weights<opt_fc_layer_t>(32, 32, 8,  8, 4, 1);
-		fc_test_fix_weights<opt_fc_layer_t>(64, 64, 16, 8, 8, 1);
-
-		fc_test<opt_fc_layer_t>(4,  4,  4,  4, 4, 1);
-		fc_test<opt_fc_layer_t>(4,  4,  2,  2, 8, 1);
-		fc_test<opt_fc_layer_t>(8,  8,  2,  2, 16,1);
-		fc_test<opt_fc_layer_t>(32, 32, 8,  8, 4, 1);
-		fc_test<opt_fc_layer_t>(64, 64, 16, 8, 8, 1);
-#endif
+#define FC_TEST_1(method)						\
+		method<opt_fc_layer_t>(4,  4,  4,  4, 4, 1);\
+		method<opt_fc_layer_t>(8,  8,  2,  2, 16,1);\
+		method<opt_fc_layer_t>(32, 32, 16, 8, 8, 1);
 		
+		FC_TEST_1(fc_test_activate);
+		FC_TEST_1(fc_test_calc_grads);
+		FC_TEST_1(fc_test_fix_weights);
+		FC_TEST_1(fc_test);
 	}
 	TEST_F(OptimizationTests, level_2_fc) {
-		fc_test_activate<opt_fc_layer_t>(4,  6,  6,  6,  6,  1);
-		fc_test_activate<opt_fc_layer_t>(4,  8,  2,  2,  2,  1);
-		fc_test_activate<opt_fc_layer_t>(12, 12, 3,  2,  3,  1);
-		fc_test_activate<opt_fc_layer_t>(24, 48, 24, 12, 12, 1);
-		fc_test_activate<opt_fc_layer_t>(16, 96, 2,  2,  12, 1);
-
-#if (0)
-		fc_test_calc_grads<opt_fc_layer_t>(4,  6,  6,  6,  6,  1);
-		fc_test_calc_grads<opt_fc_layer_t>(4,  8,  2,  2,  2,  1);
-		fc_test_calc_grads<opt_fc_layer_t>(12, 12, 3,  2,  3,  1);
-		fc_test_calc_grads<opt_fc_layer_t>(24, 48, 24, 12, 12, 1);
-		fc_test_calc_grads<opt_fc_layer_t>(16, 96, 2,  2,  12, 1);
-
-		fc_test_fix_weights<opt_fc_layer_t>(4,  6,  6,  6,  6,  1);
-		fc_test_fix_weights<opt_fc_layer_t>(4,  8,  2,  2,  2,  1);
-		fc_test_fix_weights<opt_fc_layer_t>(12, 12, 3,  2,  3,  1);
-		fc_test_fix_weights<opt_fc_layer_t>(24, 48, 24, 12, 12, 1);
-		fc_test_fix_weights<opt_fc_layer_t>(16, 96, 2,  2,  12, 1);
-
-		fc_test<opt_fc_layer_t>(4,  6,  6,  6,  6,  1);
-		fc_test<opt_fc_layer_t>(4,  8,  2,  2,  2,  1);
-		fc_test<opt_fc_layer_t>(12, 12, 3,  2,  3,  1);
-		fc_test<opt_fc_layer_t>(24, 48, 24, 12, 12, 1);
-		fc_test<opt_fc_layer_t>(16, 96, 2,  2,  12, 1);
-#endif		
+#define FC_TEST_2(method)						\
+		method<opt_fc_layer_t>(4,  6,  6,  6,  6,  1);\
+		method<opt_fc_layer_t>(12, 12, 3,  2,  3,  1);\
+		method<opt_fc_layer_t>(16, 96, 2,  2,  12, 1);
+		
+		FC_TEST_2(fc_test_activate);
+		FC_TEST_2(fc_test_calc_grads);
+		FC_TEST_2(fc_test_fix_weights);
+		FC_TEST_2(fc_test);
 	}
 
 	TEST_F(OptimizationTests, level_3_fc) {
-		fc_test_activate<opt_fc_layer_t>(3,  7,  13, 3, 7,  1);
-		fc_test_activate<opt_fc_layer_t>(5,  9,  17, 5, 11, 1);
-		fc_test_activate<opt_fc_layer_t>(31, 29, 5,  5, 13, 1);
-		fc_test_activate<opt_fc_layer_t>(89, 31, 7,  7, 19, 1);
-		fc_test_activate<opt_fc_layer_t>(3,  17, 31, 3, 23, 1);
+#define FC_TEST_3(method)						\
+		method<opt_fc_layer_t>(3,  7,  13, 3, 7,  1);\
+		method<opt_fc_layer_t>(31, 29, 5,  5, 13, 1);\
+		method<opt_fc_layer_t>(3,  17, 31, 3, 23, 1);
 		
-#if (0)
-		fc_test_calc_grads<opt_fc_layer_t>(3,  7,  13, 3, 7,  1);
-		fc_test_calc_grads<opt_fc_layer_t>(5,  9,  17, 5, 11, 1);
-		fc_test_calc_grads<opt_fc_layer_t>(31, 29, 5,  5, 13, 1);
-		fc_test_calc_grads<opt_fc_layer_t>(89, 31, 7,  7, 19, 1);
-		fc_test_calc_grads<opt_fc_layer_t>(3,  17, 31, 3, 23, 1);
-
-		fc_test_fix_weights<opt_fc_layer_t>(3,  7,  13, 3, 7,  1);
-		fc_test_fix_weights<opt_fc_layer_t>(5,  9,  17, 5, 11, 1);
-		fc_test_fix_weights<opt_fc_layer_t>(31, 29, 5,  5, 13, 1);
-		fc_test_fix_weights<opt_fc_layer_t>(89, 31, 7,  7, 19, 1);
-		fc_test_fix_weights<opt_fc_layer_t>(3,  17, 31, 3, 23, 1);
-
-		fc_test<opt_fc_layer_t>(3,  7,  13, 3, 7,  1);
-		fc_test<opt_fc_layer_t>(5,  9,  17, 5, 11, 1);
-		fc_test<opt_fc_layer_t>(31, 29, 5,  5, 13, 1);
-		fc_test<opt_fc_layer_t>(89, 31, 7,  7, 19, 1);
-		fc_test<opt_fc_layer_t>(3,  17, 31, 3, 23, 1);
-#endif
-		
+		FC_TEST_3(fc_test_activate);
+		FC_TEST_3(fc_test_calc_grads);
+		FC_TEST_3(fc_test_fix_weights);
+		FC_TEST_3(fc_test);
 	}
 
 	TEST_F(OptimizationTests, level_4_fc) {
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 10; i++) {
 			srand(i);
 			int x = RAND_LARGE(16);
 			int y = RAND_LARGE(24);
@@ -116,11 +66,10 @@ namespace Tests {
 			int out = RAND_LARGE(8);
 			
 			fc_test_activate<opt_fc_layer_t>(x,y,z,b,out,1);
-#if (0)
 			fc_test_calc_grads<opt_fc_layer_t>(x,y,z,b,out,1);
 			fc_test_fix_weights<opt_fc_layer_t>(x,y,z,b,out,1);
 			fc_test<opt_fc_layer_t>(x,y,z,b,out,1);
-#endif
+
 		}
 		
 	}
