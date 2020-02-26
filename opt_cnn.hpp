a46bda8d3464c26e70e1025969ffc588e7c1f51e
Thomas Tran <tat022@ucsd.edu>
2020-02-25 17:08:57 -0800

    Update opt_cnn.hpp

--- opt_cnn.hpp
+++ opt_cnn.hpp
@@ -46,7 +46,50 @@ class opt_fc_layer_t : public fc_layer_t
 {
 public:
 	opt_fc_layer_t( tdsize in_size, int out_size ) : fc_layer_t(in_size, out_size) {
+		void activate( tensor_t<double>& in ) {
+			copy_input(in);
 
+			tdsize old_size = in.size;
+			tdsize old_out_size = out.size;
+
+			// cast to correct shape
+			in.size.x = old_size.x * old_size.y * old_size.z;
+			in.size.y = old_size.b;
+			in.size.z = 1;
+			in.size.b = 1;
+
+			out.size.x = old_out_size.x * old_out_size.y * old_out_size.z;
+			out.size.y = old_out_size.b;
+			out.size.z = 1;
+			out.size.b = 1;
+
+			for ( int b = 0; b < activator_input.size.b; b++) {
+				for ( int n = 0; n < activator_input.size.x; n++ ) {
+					activator_input(n, 0, 0, b) = 0;
+				}
+			}
+
+			for ( int b = 0; b < in.size.y; b++ ) {
+				for ( int i = 0; i < in.size.x; i++ ) {
+					for ( int n = 0; n < out.size.x; n++ ) {
+						double in_val = in(i, b, 0);
+						double weight_val = weights( i, n, 0 );
+						double mul_val = in_val * weight_val;
+						double acc_val = activator_input(n, 0, 0, b) + mul_val;
+						activator_input(n, 0, 0, b) = acc_val;
+					}
+				}
+			}
+
+			// finally, apply the activator function.
+			for ( unsigned int n = 0; n < activator_input.element_count(); n++ ) {
+				out.data[n] = activator_function( activator_input.data[n] );
+			}
+
+			// don't forget to reset the shapes
+			in.size = old_size;
+			out.size = old_out_size;
+		}
 	}
 
 			
